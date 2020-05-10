e8ef3260cec12abb2d905c4c607ba705908aa8c1
Andoree <and2900@inbox.ru>
2020-05-11 00:51:32 +0300

    Covid tweets reformatting script

---
+++ scripts/reformat_covid_tweets.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def main():
+    covid_tweets_path = r"../covid_tweets.csv"
+    output_path = "../cleaned_covid_tweets.csv"
+    covid_tweets_df = pd.read_csv(covid_tweets_path, )
+
+    covid_tweets_df.text = covid_tweets_df.text.apply(lambda x: x.replace("\n", " ").replace("\t", " "))
+    covid_tweets_df.drop(['date', 'resource'], axis=1, inplace=True)
+    covid_tweets_df.to_csv(output_path, index=False, sep="\t", quoting=3)
+
+
+
+if __name__ == '__main__':
+    main()
