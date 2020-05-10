634b01b6a38c0a189fbe5c43fe814fb077c86b07
Andoree <and2900@inbox.ru>
2020-05-10 20:03:51 +0300

    Fixed test sets. Added zeros

--- scripts/merge_labels_and_text.py
+++ scripts/merge_labels_and_text.py
@@ -2,14 +2,16 @@ import os
 
 import numpy
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def main():
-    test_labels_path = r"not_split_all_eng_tweets/test/labels.txt"
-    test_texts_path = r"not_split_all_eng_tweets/test/text.txt"
-    train_labels_path = r"not_split_all_eng_tweets/train/labels.txt"
-    train_texts_path = r"not_split_all_eng_tweets/train/text.txt"
-    result_dir = "corpora/corpus_full_eng/"
+    test_labels_path = r"../corpora/not_split_all_eng_tweets/test/labels.txt"
+    test_texts_path = r"../corpora/not_split_all_eng_tweets/test/text.txt"
+    train_labels_path = r"../corpora/not_split_all_eng_tweets/train/labels.txt"
+    train_texts_path = r"../corpora/not_split_all_eng_tweets/train/text.txt"
+    result_dir = "../new_corpora/corpus_en_ones/"
+    negative_proportion = 0
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
 
@@ -28,19 +30,34 @@ def main():
             train_texts.append(line.strip())
     train_texts = numpy.array(train_texts)
     train_labels_df['tweet'] = train_texts
-    train_positive_class_df = train_labels_df[train_labels_df['class'] == 1]
-    train_negative_class_df = train_labels_df[train_labels_df['class'] == 0]
+    train_labels_df = pd.concat([train_labels_df, test_labels_df])
+    train_df, test_df, _, _ = \
+        train_test_split(train_labels_df, train_labels_df, test_size=0.2, random_state=42)
+    train_df, dev_df, _, _ = \
+        train_test_split(train_df, train_df, test_size=0.1, random_state=42)
+
+    train_positive_class_df = train_df[train_df['class'] == 1]
+    train_negative_class_df = train_df[train_df['class'] == 0]
     num_positive_examples = train_positive_class_df.shape[0]
-    num_negative_examples = num_positive_examples * 2
+    num_negative_examples = num_positive_examples * negative_proportion
     train_negative_class_df = train_negative_class_df.sample(num_negative_examples, )
-    class_normalized_train_df = pd.concat([train_positive_class_df, train_negative_class_df]).sample(frac=1)
+    normalized_train_df = pd.concat([train_positive_class_df, train_negative_class_df]).sample(frac=1)
+
+
+    # train_positive_class_df = train_labels_df[train_labels_df['class'] == 1]
+    # train_negative_class_df = train_labels_df[train_labels_df['class'] == 0]
+    # num_positive_examples = train_positive_class_df.shape[0]
+    # num_negative_examples = num_positive_examples * negative_proportion
+    # train_negative_class_df = train_negative_class_df.sample(num_negative_examples, )
+    # class_normalized_train_df = pd.concat([train_positive_class_df, train_negative_class_df]).sample(frac=1)
 
     output_train_path = os.path.join(result_dir, "train.tsv")
     output_test_path = os.path.join(result_dir, "test.tsv")
-    class_normalized_train_df.to_csv(output_train_path, sep='\t', encoding="utf-8", index=False)
-    test_labels_df.to_csv(output_test_path, sep='\t', encoding="utf-8", index=False)
-    print(test_labels_df)
-    print(train_labels_df)
+    output_dev_path = os.path.join(result_dir, "dev.tsv")
+    normalized_train_df.to_csv(output_train_path, sep='\t', encoding="utf-8", index=False, header=None)
+    test_df.to_csv(output_test_path, sep='\t', encoding="utf-8", index=False)
+    dev_df.to_csv(output_dev_path, sep='\t', encoding="utf-8", index=False, header=None)
+    print(normalized_train_df)
 
 
 if __name__ == '__main__':
--- scripts/reformat.py
+++ scripts/reformat.py
@@ -7,9 +7,11 @@ COLUMN_ORDER = ["class", "tweet"]
 
 
 def main():
-    training_path = r"corpora/not_split_all_ru_tweets/task2_ru_training.tsv"
-    val_path = r"corpora/not_split_all_ru_tweets/task2_ru_validation.tsv"
-    out_dir = r"corpora/corpus_2_1_ru/"
+    training_path = r"../corpora/not_split_all_ru_tweets/task2_ru_training.tsv"
+    val_path = r"../corpora/not_split_all_ru_tweets/task2_ru_validation.tsv"
+    out_dir = r"../new_corpora/corpus_ru_ones/"
+    negative_proportion = 0
+
     # training_path = r"corpus_full_eng/train.tsv"
     # val_path = r"corpus_full_eng/test.tsv"
     # out_dir = r"english_tweets_normalized"
@@ -27,7 +29,7 @@ def main():
     train_positive_class_df = train_df[train_df['class'] == 1]
     train_negative_class_df = train_df[train_df['class'] == 0]
     num_positive_examples = train_positive_class_df.shape[0]
-    num_negative_examples = num_positive_examples * 2
+    num_negative_examples = num_positive_examples * negative_proportion
     train_negative_class_df = train_negative_class_df.sample(num_negative_examples, )
     print("Positive", train_positive_class_df)
     print("Negative", train_negative_class_df)
@@ -37,7 +39,6 @@ def main():
     out_train_path = os.path.join(out_dir, "train.tsv")
     out_test_path = os.path.join(out_dir, "test.tsv")
     out_dev_path = os.path.join(out_dir, "dev.tsv")
-
     class_normalized_train_df.to_csv(out_train_path, sep="\t", encoding="utf-8", quoting=3, index=False, quotechar=None)
     test_df.to_csv(out_test_path, sep="\t", encoding="utf-8", index=False, quoting=3, quotechar=None)
     dev_df.to_csv(out_dev_path, sep="\t", encoding="utf-8", index=False, quoting=3,quotechar=None)
