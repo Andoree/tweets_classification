4f7bf569327b745abfeacb4b798ce4b9bce0c032
Andoree <and2900@inbox.ru>
2020-05-17 18:41:00 +0000

    Marked up bert features json

--- scripts/transform_bert_features.py
+++ scripts/transform_bert_features.py
@@ -3,11 +3,27 @@ import json
 
 def main():
     bert_features_json_path = r"../../bert/bert_features/dictionary.json"
-    with codecs.open(bert_features_json_path, "r", encoding="utf-8") as inp:
-        for line in inp:
-            json_doc = json.load(line)
+    with codecs.open(bert_features_json_path, "r",) as inp:
+        for i in range(10):
+            line = inp.readline()
+            json_doc = json.loads(line)
             for k, v in json_doc.items():
-                print(k, type(v))
+                print(i, k,type( v))
+                if k == "linex_index":
+                    print(v)
+                elif k == "features":
+                    for elem in v:
+                        # dictionary with keys [token, layers]
+                        print('aa', elem.keys())
+                        layers = elem['layers']
+			# list of layers
+                        print("layers", type(layers))
+                        for j in layers:
+                            # single layer: dict with keys[index, embedding]
+                            print("bbbbbbb", j.keys())
+                            print(j['index'])
+                            print(len(j['values']))
+
 
 
 if __name__ == '__main__':
