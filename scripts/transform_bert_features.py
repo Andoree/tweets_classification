ce9703f3feae20cffcb0f83d5d8036cf4976a5d0
An <an@clab>
2020-05-18 20:40:54 +0300

    Script for merging layers into one token embedding

--- scripts/transform_bert_features.py
+++ scripts/transform_bert_features.py
@@ -1,38 +1,43 @@
 import codecs
 import json
+import numpy as np
+IGNORED_TOKENS = ['[SEP]',]
+
 
 def main():
     bert_features_json_path = r"../../bert/bert_features/rudrec.json"
-    with codecs.open(bert_features_json_path, "r",encoding="utf-8") as inp:
+    output_file_path = "tweets_tokens_embeddings.txt"
+    with codecs.open(bert_features_json_path, "r", encoding="utf-8") as inp, \
+            codecs.open(output_file_path, "w+", encoding="ascii") as output:
         for i in range(2):
             print('----' * 10)
             line = inp.readline()
             json_doc = json.loads(line)
             for k, v in json_doc.items():
-                # print(i, k,type( v))
-                #print('--' * 10)
                 if k == "linex_index":
-                    print(v)
+                    print('linex_index', v)
+                # Processing of individual token
                 elif k == "features":
+                    num_tokens = len(v)
+                    print('num_tokens', num_tokens)
                     for elem in v:
                         token = elem['token']
-                        #if token == '[CLS]':
-                            #print(token)
-                        # dictionary with keys [token, layers]
-                        elem['token']
-                        print('\taa', elem.keys())
-                        layers = elem['layers']
-			# list of layers
-                        print('\telem.token', elem['token'])
-                        print("\tlayers", type(layers))
-                        for j in layers:
-                            # single layer: dict with keys[index, embedding]
-                            print("\t\tbbbbbbb", j.keys())
-                            print('\t\tIndex',j['index'])
-                            print( '\t\t len vectors',len(j['values']))
+                        if token not in IGNORED_TOKENS:
+                            token_embedding = np.zeros(shape=768, dtype=np.float32)
+                            layers = elem['layers']
+                            num_layers = len(layers)
+                            print('num_layers', num_layers)
+                            for layer in layers:
+                                # single layer: dict with keys[index, embedding]
+                                layer_embedding = layer['values']
+                                layer_embedding = np.array(layer_embedding, dtype=np.float32)
+                                token_embedding += layer_embedding
+                            token_embedding /= num_layers
+                            token_embedding_string = ','.join((str(x) for x in token_embedding))
+                            output.write(f"{token_embedding_string}\t")
+                    output.write('\n')
 
 
 
 if __name__ == '__main__':
     main()
-
