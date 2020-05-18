5f57376149ba30faf5a02404dce0df08abc4f850
Andoree <and2900@inbox.ru>
2020-05-18 16:06:22 +0000

    Transform bert features script

--- scripts/transform_bert_features.py
+++ scripts/transform_bert_features.py
@@ -2,27 +2,34 @@ import codecs
 import json
 
 def main():
-    bert_features_json_path = r"../../bert/bert_features/dictionary.json"
-    with codecs.open(bert_features_json_path, "r",) as inp:
-        for i in range(10):
+    bert_features_json_path = r"../../bert/bert_features/rudrec.json"
+    with codecs.open(bert_features_json_path, "r",encoding="utf-8") as inp:
+        for i in range(2):
+            print('----' * 10)
             line = inp.readline()
             json_doc = json.loads(line)
             for k, v in json_doc.items():
-                print(i, k,type( v))
+                # print(i, k,type( v))
+                #print('--' * 10)
                 if k == "linex_index":
                     print(v)
                 elif k == "features":
                     for elem in v:
+                        token = elem['token']
+                        #if token == '[CLS]':
+                            #print(token)
                         # dictionary with keys [token, layers]
-                        print('aa', elem.keys())
+                        elem['token']
+                        print('\taa', elem.keys())
                         layers = elem['layers']
 			# list of layers
-                        print("layers", type(layers))
+                        print('\telem.token', elem['token'])
+                        print("\tlayers", type(layers))
                         for j in layers:
                             # single layer: dict with keys[index, embedding]
-                            print("bbbbbbb", j.keys())
-                            print(j['index'])
-                            print(len(j['values']))
+                            print("\t\tbbbbbbb", j.keys())
+                            print('\t\tIndex',j['index'])
+                            print( '\t\t len vectors',len(j['values']))
 
 
 
